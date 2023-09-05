64722c37b512b81a9cff325e700b3bb7221fff4a
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-09-05 21:12:54 +0300

    working with while loops

---
+++ loops/while.py
@@ -0,0 +1,15 @@
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+
+
+# While Loop with Else
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+else:
+    print("Loop finished")
+
+# In this example, we use a while loop to repeatedly execute a block of code as long as the condition i < 5 is true. We initialize the variable i to 0, and then increment it by 1 on each iteration of the loop. We print the value of i to the console on each iteration. Once the loop has finished, we print the message "Loop finished" to the console using the else statement.
