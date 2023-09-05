b968c447e2c0516a1b6e5f38024814733b0a8443
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-09-05 21:11:34 +0300

    working with tuples

---
+++ datatypes/tuples.py
@@ -0,0 +1,6 @@
+coordinates = (3, 5)
+print(coordinates[0])  # Output: 3
+
+# The main difference between tuples and lists in Python is that tuples are immutable, while lists are mutable.
+
+# This means that once you create a tuple, you cannot change its contents. 
