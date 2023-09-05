67ee169ab7499201102a1b8471d1aa9ffde2ee12
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-09-05 21:02:10 +0300

    working with variables

---
+++ variables.py
@@ -0,0 +1,19 @@
+# defining
+x = 5
+y = 10
+name = "John"
+
+
+# Variable Types
+x = 5
+y = 3.14
+name = "John"
+is_student = True
+
+# In this example, we define four variables x, y, name, and is_student. The variable x is an integer, y is a float, name is a string, and is_student is a boolean.
+
+# Type Conversion
+x = 5
+y = "10"
+z = x + int(y)
+print(z)  # Output: 15
