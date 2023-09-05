d26e6abe6fa10396518936885089c8e68ff9c9cd
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-09-05 21:09:10 +0300

    working with dictionaries

---
+++ datatypes/dictionaries.py
@@ -0,0 +1,4 @@
+person = {"name": "John", "age": 30, "city": "New York"}
+print(person["age"])  # Output: 30
+person["age"] = 35
+print(person)  # Output: {"name": "John", "age": 35, "city": "New York"}
