162ab9f4a1a8dedefc63951c105216ab64bd9a6e
Halefom Alemu (Haniel) <hannelalex12@gmail.com>
2023-09-05 21:15:22 +0300

    working with functions

---
+++ function.py
@@ -0,0 +1,14 @@
+def greet(name):
+    print("Hello, " + name + "!")
+
+greet("halefom")
+
+
+# Returning a Value
+def add(x, y):
+    return x + y
+
+
+# Using a Returned Value
+result = add(3, 5)
+print(result)  # Output: 8
